acfa1c50718d70aef5e09fad3472e63fba47aa94
bryangarcia <alexisgarciathb1996@gmail.com>
2025-10-25 10:04:15 -0600

    Initial Commit

---
+++ cicdAppnew/cicdAppnewApp.swift
@@ -0,0 +1,17 @@
+//
+//  cicdAppnewApp.swift
+//  cicdAppnew
+//
+//  Created by bryangarcia on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct cicdAppnewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
