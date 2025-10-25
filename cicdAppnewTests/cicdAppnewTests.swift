4f008031057d12d66a0a346139b42162427c20c1
bryangarcia <alexisgarciathb1996@gmail.com>
2025-10-25 10:47:16 -0600

    add

---
+++ cicdAppnewTests/cicdAppnewTests.swift
@@ -0,0 +1,16 @@
+//
+//  cicdAppnewTests.swift
+//  cicdAppnewTests
+//
+//  Created by bryangarcia on 25/10/25.
+//
+
+import Testing
+
+struct cicdAppnewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
